425f535a6fc33ee4ac4a99be7b25f16e6d3b3ba7
Asif <asif@venzotechnologies.com>
2021-11-22 16:40:55 +0530

    add basic post and put routes

--- src/controllers/ApiController.ts
+++ src/controllers/ApiController.ts
@@ -8,7 +8,7 @@ class ApiController {
             console.log(req.params.tablename)
             const client = await pool.connect();
             const table = req.params.tablename
-            const sql = `SELECT * FROM ${table}`;
+            const sql = `SELECT * FROM ${table} ORDER BY id ASC`;
             const { rows } = await client.query(sql);
             const todos = rows;
             console.log(todos)
@@ -18,6 +18,51 @@ class ApiController {
             res.status(400).send(error);
         }
     }
+    
+    public async post (req: Request, res: Response) {
+        try {
+          const table = req.params.tablename
+          const client = await pool.connect();
+          const { name, city, country_code, base_currency } = req.body;
+          const query = `INSERT INTO ${table}(name, city, country_code, base_currency) VALUES('${name}','${city}','${country_code}','${base_currency}') RETURNING *`;
+        
+          const { rows } = await client.query(query)
+          const data = rows
+          console.log(data)
+          client.release();
+          res.status(201).send({
+            status: 'Created',
+            result: data,
+          });
+        } catch (error) {
+            res.status(400).send(error)
+        }
+    }
+
+    public async put (req: Request, res: Response) {
+        try {
+          const table = req.params.tablename
+          const id = req.params.recordid
+          const client = await pool.connect();
+          const select = `SELECT * FROM ${table} WHERE id = ${id}`
+          var {rows} = await client.query(select)
+          const { name = rows[0].name, city = rows[0].city, country_code = rows[0].country_code, base_currency = rows[0].base_currency } = req.body;
+          const updatequery = `UPDATE ${table}
+                        SET name='${name}', city = '${city}', country_code = '${country_code}', base_currency = '${base_currency}'
+                        WHERE id = ${id}`;
+        
+          await client.query(updatequery)
+          var {rows} = await client.query(select) 
+          console.log(rows)
+          client.release();
+          res.status(201).send({
+            status: 'UPDATED',
+            result: rows,
+          });
+        } catch (error) {
+            res.status(400).send(error)
+        }
+    };
 }
 
 export default ApiController;
--- src/routers/ApiRouter.ts
+++ src/routers/ApiRouter.ts
@@ -5,5 +5,7 @@ const router = Router();
 const apiController = new ApiController();
 
 router.get('/:tablename', apiController.get);
+router.post('/:tablename', apiController.post);
+router.put('/:tablename/:recordid', apiController.put)
 
 export default router;
